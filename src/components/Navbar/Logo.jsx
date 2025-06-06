be2c395edd141157420c323a658372c6d87fb917
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-06 14:51:11 -0500

    feat: change text to english

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -6,7 +6,7 @@ export const Logo = () => {
   return (
     <div className="flex h-navbar-items items-center gap-2 bg-black border-border border-2 p-2 px-3 rounded-full">
       <Popcorn />
-      <h1 className="text-xl">Buscador de películas</h1>
+      <h1 className="text-xl">Movie Search</h1>
     </div>
   );
 };
