6e12460155294e0aa302268e8ca821b4be7c3ca0
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-06 14:52:23 -0500

    feat: animations in hover search and theme

--- src/components/Navbar/Theme.jsx
+++ src/components/Navbar/Theme.jsx
@@ -5,9 +5,9 @@ export const Theme = () => {
   const theme = localStorage.getItem("theme");
 
   return (
-    <div className="flex items-center justify-center h-navbar-items w-navbar-items">
-      <button className="bg-black border-border border-2 p-2 rounded-full cursor-pointer">
-        {theme === "dark" ? <Sun /> : <Moon />}
+    <div className="flex items-center justify-center h-navbar-items w-navbar-items group">
+      <button className="bg-black border-border border-2 p-2 rounded-full cursor-pointer hover:bg-border hover:border-black transition-colors">
+        {theme === "dark" ? <Sun className="group-hover:animate-squeeze" /> : <Moon className="group-hover:animate-squeeze" />}
       </button>
     </div>
   );
