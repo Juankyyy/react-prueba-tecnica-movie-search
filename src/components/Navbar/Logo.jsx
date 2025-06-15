a6d391b71ff9ef5ffcc7724b432ef3db3d546c4f
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-15 13:56:46 -0500

    feat: padding search input and logo pointer

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -4,7 +4,7 @@ export const Logo = () => {
   // 🧠 Logic
 
   return (
-    <div className="flex h-navbar-items items-center gap-2 bg-black border-border border-2 p-2 px-3 rounded-full">
+    <div className="flex h-navbar-items items-center gap-2 bg-black border-border border-2 p-2 px-3 rounded-full cursor-pointer">
       <Popcorn />
       <h1 className="text-xl">Movie Search</h1>
     </div>
--- src/components/Navbar/Search.jsx
+++ src/components/Navbar/Search.jsx
@@ -4,15 +4,15 @@ export const Search = () => {
   // 🧠 Logic
 
   return (
-    <div className="flex items-center group gap-2 h-navbar-items bg-black border-border hover:bg-border hover:border-black border-2 p-2 px-3 rounded-full transition-colors">
+    <form className="flex items-center group gap-2 p-2 px-3 h-navbar-items bg-black border-border hover:bg-border hover:border-black border-2 rounded-full transition-colors">
       <input
-        className="focus:outline-0 px-2"
+        className="focus:outline-0 p-2 px-3"
         type="text"
         placeholder="Elysium, Interstellar..."
       />
       <button className="group-hover:animate-squeeze">
         <SearchIcon className="cursor-pointer" />
       </button>
-    </div>
+    </form>
   );
 };
