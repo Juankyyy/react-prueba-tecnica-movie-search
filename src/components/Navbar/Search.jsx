055358c8e8fd4d19d7f738f2d6918378a8b8e4f1
Juankyyy <juancamiloherrera960@gmail.com>
2025-06-15 14:14:05 -0500

    feat: onchange input value and submit value

--- src/components/Navbar/Search.jsx
+++ src/components/Navbar/Search.jsx
@@ -1,14 +1,26 @@
 import { SearchIcon } from "lucide-react";
+import { useState } from "react";
 
 export const Search = () => {
-  // 🧠 Logic
+  const [query, setQuery] = useState("");
+
+  const onInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(query);
+  };
 
   return (
-    <form className="flex items-center group gap-2 p-2 px-3 h-navbar-items bg-black border-border hover:bg-border hover:border-black border-2 rounded-full transition-colors">
+    <form onSubmit={onSubmit} className="flex items-center group gap-2 p-2 px-3 h-navbar-items bg-black border-border hover:bg-border hover:border-black border-2 rounded-full transition-colors">
       <input
         className="focus:outline-0 p-2 px-3"
         type="text"
         placeholder="Elysium, Interstellar..."
+        value={query}
+        onChange={onInputChange}
       />
       <button className="group-hover:animate-squeeze">
         <SearchIcon className="cursor-pointer" />
